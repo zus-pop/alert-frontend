077ca78be85bb8bdbea796f0bc2da3303e07e3d0
Minh Khoa <khoa1509gg@gmail.com>
2025-07-23 07:59:39 +0700

    Update page.tsx add console

--- app/admin/majors/page.tsx
+++ app/admin/majors/page.tsx
@@ -109,6 +109,7 @@ export default function MajorListPage() {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200">
+               {console.log("111",data)}
               {data?.data?.map((major: Major) => (
                 <tr key={major._id} className="hover:bg-gray-50">
                   <td className="px-6 py-4 text-sm text-gray-800 font-medium">{major.majorCode}</td>
